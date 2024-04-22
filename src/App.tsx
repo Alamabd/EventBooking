0fd36394d6d9ac8a7eb37db080b3ace591f0a4aa
nifalq <asuakun157@gmail.com>
2024-04-22 10:03:48 +0700

    feat: added routes for bottom tab

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import React from 'react';
 import First from './routes/First';
 import Signup from './routes/Signup';
 import Setloc from './routes/Setloc';
+
+import Home from './routes/tab/Home';
+import Explore from './routes/tab/Explore';
+import Favorite from './routes/tab/Favorite';
+import Ticket from './routes/tab/Ticket';
+import Profile from './routes/tab/Profile';
+import { Image } from 'react-native';
 
 export type StackParamsList = {
   First: string,
@@ -13,6 +21,19 @@ export type StackParamsList = {
 function App() {
 
   const Stack = createNativeStackNavigator();
+  const Tab = createBottomTabNavigator();
+
+  function TabBottom(): React.JSX.Element {
+    return(
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={Home}/>
+        <Tab.Screen name="Explore" component={Explore} />
+        <Tab.Screen name="Favorite" component={Favorite} />
+        <Tab.Screen name="Ticket" component={Ticket} />
+        <Tab.Screen name="Profile" component={Profile} />
+      </Tab.Navigator>
+    )
+  }
 
   return (
     <NavigationContainer>
@@ -43,6 +64,11 @@ function App() {
             headerTransparent: true
           }
         } name="Setloc" component={Setloc} />
+        <Stack.Screen options={
+          {
+            headerShown: false,
+          }
+         } name="TabBottom" component={TabBottom} />
       </Stack.Navigator>
     </NavigationContainer>
   );
---
+++ src/routes/tab/Favorite.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+function Favorite(): React.JSX.Element {
+  return (
+    <View>
+      <Text>Favorite</Text>
+    </View>
+  )
+}
+
+export default Favorite;
